8cbca006b5b3251e9b3aaf7c9b453cb8135f5ee4
rakshith22-glitch <rakshith22@outlook.com>
2024-05-25 10:13:59 -0700

    getallusers

--- controllers/userController.js
+++ controllers/userController.js
@@ -99,12 +99,12 @@ export const getUserById = async (req, res) => {
     }
 };
 
-export const getUsers = async (req, res) => {
+export const getAllUsers = async (req, res) => {
     try {
         const users = await User.find().select('-password');
         res.json(users);
     } catch (error) {
-        console.error('Error fetching users:', error.message);
-        res.status(500).json({ message: "Error fetching users", error: error.message });
+        console.error('Error fetching all users:', error.message);
+        res.status(500).json({ message: "Error fetching all users", error: error.message });
     }
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,17 @@
 
 import express from 'express';
-import { registerUser, becomeMember, cancelMembership, getUserProfile, loginUser, getUserById, getUsers } from '../controllers/userController.js';
+import { getAllUsers, registerUser, becomeMember, cancelMembership, getUserProfile, loginUser, getUserById } from '../controllers/userController.js';
 import { protect } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
-
+router.get('/', getAllUsers);
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/profile', protect, getUserProfile);
 router.put('/become-member', protect, becomeMember);
 router.put('/cancel-membership', protect, cancelMembership);
 router.route('/:userId').get(getUserById);
-router.get('/allusers', getUsers);
+
 
 
 export default router;
